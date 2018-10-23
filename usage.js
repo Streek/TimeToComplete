1c8cb7fe9c52663b60dec64560e9af65723fbd1f
Keith Connolly <streek@mesaro.com>
2018-10-23 13:21:17 -0700

    update usage

--- usage.js
+++ usage.js
@@ -3,12 +3,10 @@ const time = new TimeToComplete(100);
 
 setTimeout(() => {
   time.remaining(40, true);
-}, 1000)
-
-setTimeout(() => {
-  time.remaining(44, true);
-}, 1000)
-
-setTimeout(() => {
-  time.remaining(48, true);
+  setTimeout(() => {
+    time.remaining(44, true);
+    setTimeout(() => {
+      time.remaining(48, true);
+    }, 1000)
+  }, 1000)
 }, 1000)
